4777d58d6cab2a6f438586c9e83409ef3223bff9
lifei <lllpla@163.com>
2018-03-20 15:30:11 +0800

    生产环境配置修改

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask_restful import  Api
 from src.apps import app
 from src.views import PersonView, PersonListView
+from werkzeug.contrib.fixers import ProxyFix
 
+from src.apps import app, DEV_FLAG
 api = Api(app)
 api.add_resource(PersonView, '/person/<person_id>')
 api.add_resource(PersonListView, '/person')
 
 # Start the flask loop
 if __name__ == '__main__':
-    app.run()
-
+    if DEV_FLAG:
+        app.run()
+    else:
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+        app.run(host='0.0.0.0',port="1111")
--- src/apps.py
+++ src/apps.py
@@ -4,3 +4,5 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
+
+DEV_FLAG = True
